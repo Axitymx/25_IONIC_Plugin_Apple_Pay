23e877a3d8d75e4b7f12a3eb2f03c4001c3eb8bb
Daniel-Aguilar-Ojeda <147107094+Daniel-Aguilar-Ojeda@users.noreply.github.com>
2025-09-30 16:41:54 -0600

    refactor: eliminación de la carpeta dist

--- dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const ApplePaySession = registerPlugin('ApplePaySession', {
-    web: () => import('./web').then((m) => new m.ApplePaySessionWeb()),
-});
-export * from './definitions';
-export { ApplePaySession };
-//# sourceMappingURL=index.js.map
